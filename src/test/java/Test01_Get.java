f2b13c4d40c0865df3578004864689b4cd6d9444
gourav.jain <gourav.b.jain@accenture.com>
2021-06-07 18:52:00 +0530

    Updating commit

--- src/test/java/Test01_Get.java
+++ src/test/java/Test01_Get.java
@@ -7,12 +7,14 @@ public class Test01_Get {
 	@Test
 	public void test01() {
 
+		// Hitting the get request and getting the response.
 		Response response = RestAssured.get("https://reqres.in/api/users?page=2");
 
 		System.out.println(response.getStatusCode());
 		System.out.println(response.getBody());
 		System.out.println(response.asString());
 		System.out.println(response.getStatusLine());
+		System.out.println();
 
 		System.out.println(response.asString());
 	}
