0edd7d6417490087952c31f8764b8b10946f80fb
Gourav Jain <jaingourav999@gmail.com>
2021-06-07 18:08:00 +0530

    addingall

---
+++ src/test/java/Test01_Get.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+import io.restassured.*;
+import io.restassured.response.Response;
+
+public class Test01_Get {
+
+	@Test
+	public void test01() {
+
+		Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getBody());
+		System.out.println(response.asString());
+		System.out.println(response.getStatusLine());
+
+		System.out.println(response.asString());
+	}
+}
